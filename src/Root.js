72eda99f1681f7f522e294ccf78ee0200cb2b391
ParkMD7 <parkermdinsmore@gmail.com>
2019-02-09 15:35:40 -0500

    added custom async action creator middleware with similar functionality to redux-promise

--- src/Root.js
+++ src/Root.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 
+
 // user files
 import reducers from './reducers';
+import async from './middlewares/async';
 
 
 export default ({children, initialState = {}}) => {
-  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise))
+  const store = createStore(reducers, initialState, applyMiddleware(async))
 
   return(
     <Provider store={store}>
---
+++ src/middlewares/async.js
@@ -0,0 +1,28 @@
+// NOTE: this file will house custom middleware
+// NOTE: we will build async action creators -> action creators that return a promise on its payload property
+
+// NOTE: for the first function will be called with the dispatch property
+// export default function({ dispatch }){
+  // this function will be called with the 'next' function -> sends it to the next middleware on the stack
+  // return function(next){
+    // this function will be called with our returned action obj
+    // return function(action){
+//
+//     }
+//   }
+// }
+
+// Refactored above -> 100% the exact same
+export default ({ dispatch }) => (next) => (action) => {
+  // Here is where our middleware logic gets executed. For the purposes of this custom middleware we are checking to see if the action contains a Promise on its payload property
+  // If it does, we then want to wait for it to resolve and if it doesn't then we send it on to the next middleware on our stack
+  if(!action.payload || !action.payload.then){
+    return next(action)
+  }
+
+  // if we get to this part of the code then it means our action obj has a Promise
+  action.payload.then((response) => {
+    const newActionObj = { ...action, payload: response }
+    dispatch(newActionObj) // takes the newly created newActionObj and sends it back through all of our middleware - but this time with no Promise tied to it
+  })
+}
